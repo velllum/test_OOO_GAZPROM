cf7e8cc24bef052c2bd463df5faf4b0fcf88e9bf
velllum <velllum@yandex.ru>
2021-11-28 23:17:28 +0300

    Рефакторинг представления views.py

--- blog/application/views.py
+++ blog/application/views.py
@@ -1,84 +1,92 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import generic as gc
 
-from . import models
-from .forms import LoginForm, CreateArticleForm
+from django.conf import settings as st
+
+from . import models as ml
+from . import forms as fm
 
 
-class Index(ListView):
+class Index(gc.ListView):
     """- Главной страницы"""
-    paginate_by = 6
-    model = models.Article
+    paginate_by = st.NUMBER_PAGE
+    model = ml.Article
     template_name = "index.html"
     context = "Blog Application"
 
     def get_queryset(self):
-        return models.Article.objects.all()
+        """- переопределяем запрос к базе"""
+        return ml.Article.objects.all()
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        # переопределяем контекст запроса, добавляем title
         context = super().get_context_data(**kwargs)
         context["title"] = self.context
         return context
 
 
-class AuthorContent(ListView):
+class AuthorContent(gc.ListView):
     """- Все страницы автора"""
-    model = models.Article
+    model = ml.Article
     template_name = "content_author.html"
     context = "Все страницы автора"
 
     def get_queryset(self):
+        """- переопределяем запрос к базе"""
         print(self.kwargs.get("author"))
-        return models.Article.objects.filter(author=self.kwargs.get("auth_id"))
+        return ml.Article.objects.filter(author=self.kwargs.get("auth_id"))
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        # переопределяем контекст запроса, добавляем title
         context = super().get_context_data(**kwargs)
         context["title"] = self.context
         return context
 
 
-class ShowArticle(DetailView):
+class ShowArticle(gc.DetailView):
     """- Просмотр статьи"""
-    model = models.Article
+    model = ml.Article
     template_name = "show.html"
 
 
-class AuthorArticles(LoginRequiredMixin, ListView):
+class AuthorArticles(LoginRequiredMixin, gc.ListView):
     """- Статьи Автора"""
-    model = models.Article
+    model = ml.Article
     template_name = "author.html"
     context = "Статьи автора"
 
     def get_queryset(self):
-        return models.Article.objects.filter(author=self.request.user)
+        """- переопределяем запрос к базе"""
+        return ml.Article.objects.filter(author=self.request.user)
 
     def get_context_data(self, *, object_list=None, **kwargs):
+        # переопределяем контекст запроса, добавляем title
         context = super().get_context_data(**kwargs)
         context["title"] = self.context
         return context
 
 
-class DeleteArticle(LoginRequiredMixin, DeleteView):
+class DeleteArticle(LoginRequiredMixin, gc.DeleteView):
     """- Удалить статью"""
-    model = models.Article
+    model = ml.Article
     success_url = reverse_lazy('author')
     template_name = 'delete.html'
 
 
-class EditArticle(LoginRequiredMixin, UpdateView):
+class EditArticle(LoginRequiredMixin, gc.UpdateView):
     """- Редактировать статью"""
-    model = models.Article
-    form_class = CreateArticleForm
+    model = ml.Article
+    form_class = fm.CreateArticleForm
     success_url = reverse_lazy('author')
     template_name = 'edit.html'
 
 
-class CreateArticle(LoginRequiredMixin, CreateView):
+class CreateArticle(LoginRequiredMixin, gc.CreateView):
     """- Добавить новую статью"""
-    model = models.Article
-    form_class = CreateArticleForm
+    model = ml.Article
+    form_class = fm.CreateArticleForm
     success_url = reverse_lazy('create')
     template_name = 'create.html'
 
@@ -90,7 +98,7 @@ class CreateArticle(LoginRequiredMixin, CreateView):
 
 class Login(LoginView):
     """- Пройти аутентификацию"""
-    form_class = LoginForm
+    form_class = fm.LoginForm
     success_url = reverse_lazy('index')
     template_name = 'login.html'
 
--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -148,3 +148,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+NUMBER_PAGE = 6
